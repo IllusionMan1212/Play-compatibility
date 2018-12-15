703fd2215bb8f00328fe729d363f3f6f9362ddee
Hisham Abourgheba <36709480+IllusionMan1212@users.noreply.github.com>
2018-12-15 04:13:57 +0200

    Themes (#3)

---
+++ js/search.js
@@ -0,0 +1,45 @@
+function Search() {
+    Name = document.getElementById("radio");
+    Status = document.getElementById("radio1");
+    if (Name.checked) {
+        searchByName();
+    } else if (Status.checked) {
+        searchByStatus();
+    }
+};
+
+function searchByName() {
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                    }
+                }       
+            }
+        };
+
+function searchByStatus() {
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                    }
+                }       
+            }
+        };
